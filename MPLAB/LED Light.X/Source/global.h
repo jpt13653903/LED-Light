fa9945bbc7feda2cd3bf81b0d5a3227cac71f947
John-Philip Taylor <jpt13653903@gmail.com>
2019-10-26 16:29:26 +0200

    Copied schematics and MPLAB projects from FUI Audio DAC
    These are used as a starting point

---
+++ MPLAB/LED Light.X/Source/global.h
@@ -0,0 +1,43 @@
+//==============================================================================
+// Copyright (C) John-Philip Taylor
+// jpt13653903@gmail.com
+//
+// This file is part of LED Light
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//==============================================================================
+
+#ifndef global_h
+#define global_h
+//------------------------------------------------------------------------------
+
+#define byte  unsigned char
+#define word  unsigned short
+#define dword unsigned long
+
+#define bool  byte
+#define true  1
+#define false 0
+//------------------------------------------------------------------------------
+
+#define LED       PORTCbits.RC4
+#define Tx        PORTCbits.RC5
+#define Knob1     PORTAbits.RA4
+#define Knob2     PORTAbits.RA5
+#define Buttons  (PORTC & 0x0F)
+#define Interrupt PORTAbits.RA2
+//------------------------------------------------------------------------------
+
+#endif
+//------------------------------------------------------------------------------
